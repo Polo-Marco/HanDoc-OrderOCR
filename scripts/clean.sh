b9d61d1d228252c47609f9c1ccd340b61835e568
Polo-Marco <mark860709@gmail.com>
2025-06-14 16:45:47 +1200

    adjust requirements and clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,5 @@
 rm  ../output/det_results/*
-rm  ../output/rec_imgs/*
-rm  ../saved/*
-rm  ../output/order_det/*
-rm ../output/predicts_db.txt
-rm ../output/predicts_svtr_tiny_ch_all.txt
+rm  ../output/rec_images/*
+rm  ../app/saved/*
+rm  ../output/order_results/*
+rm ../output/rec_results/*
